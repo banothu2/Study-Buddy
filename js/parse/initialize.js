fe7772bc10324029fb4edbdeaa88cb519a3a9397
Kirthi Banothu <banothu2@illinois.edu>
2013-02-03 00:58:45 +0800

    Adding Initial files
    Here begins beta version

---
+++ js/parse/initialize.js
@@ -0,0 +1,1 @@
+Parse.initialize("78z9KHgSR3zIeFF7soCrOiHE6D99hNTJOmnJ2oay", "E8xzL8zlCs6GFfA2kEjAuBdedyZ9vS8ErBI3vT71");
---
+++ js/parse/login.js
@@ -0,0 +1,57 @@
+var currentUser = Parse.User.current();
+$(document).ready(
+	function(){
+		if(currentUser){
+			window.location.replace("../public/index.html");
+		}
+		else{
+						$("#recover").submit(
+							function regularLogin(){
+								Parse.User.logIn($("#inputUsername").val(), $("#inputPassword").val(), { //$("#inputUsername").val(), $("#inputPassword").val()
+									success: function(user) {
+										//if login success, then redirect page to index.html
+										window.location.replace("../docs/index.html");
+
+									},
+									error: function(user, error) {
+										//login failed - will redirect user back to the same page but will run the append line below
+								    
+								    //document.getElementById("loginError").innerHTML="<div class='alert alert-error'> The Username and password did not match! Please try again </div>";
+
+										//Login Failed - display error right now for debugging purposes
+										$("#error").empty();
+										$("#error").append("<div class='alert alert-error'> Oh snap! The username and password did not match! Try again!</div>");
+
+									}
+								}	
+								);
+								return false;
+							}
+						);
+
+						$("#login").submit(
+							function facebookLogin(){
+								Parse.FacebookUtils.logIn(null, {
+								  success: function(user) {
+								    if (!user.existed()) {
+								      //alert("User signed up and logged in through Facebook!");
+								      window.location.replace("../auth/facebookLogin.html");
+
+								    } else {
+								      //alert("User logged in through Facebook!");
+								      window.location.replace("../docs/index.html");
+
+								    }
+								  },
+								  error: function(user, error) {
+										$("#errorFacebook").empty();
+										$("#errorFacebook").append("<div class='alert alert-error'> Oh snap! The user cancelled the Facebook login or did not fully authorize! Try again!</div>");
+								    //alert("User cancelled the Facebook login or did not fully authorize.");
+								  }
+								});
+								return false;
+							}
+						);
+		}
+	}
+	);
